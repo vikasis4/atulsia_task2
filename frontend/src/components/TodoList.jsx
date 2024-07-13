91c4e98c29df0e8ba3799c005b76a42ba6ed0c9d
Vikas Singh <89316827+vikasis4@users.noreply.github.com>
2024-07-13 18:14:29 +0530

    2

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -5,25 +5,26 @@ import List from './List';
 const TodoList = () => {
 
     // Using our custom hook
-    const { data, error, isLoading, deleteTodo, updateToDo } = UseApi()
+    const { data, error, isLoading, mutationLoading, deleteTodo, updateToDo } = UseApi()
+
 
     // Memoizing our list to prevent unnecessary function call due multiple time page re-rendering. It will only get called when data param will update
     let content = React.useMemo(() => {
-        if (isLoading) {
-            return <p>Loading...</p>;
-        } else if (data?.status) {
+        if (data?.status) {
             return data.toDo.map((todo) => <List key={todo.id} todo={todo} deleteTodo={deleteTodo} updateToDo={updateToDo} />)
-        } else {
-            return <p>{error}</p>;
-        }
+        } else { return ErrorTab }
+        // eslint-disable-next-line 
     }, [data]);
 
     return (
         <div className="max-w-md mx-auto mt-8">
-            <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-            {content}
+            <h1 className="text-2xl font-bold mb-4">Todo List {mutationLoading ? Loading : null}</h1>
+            {error ? ErrorTab : isLoading ? Loading : content}
         </div>
     );
 };
+
+const Loading = <span className='text-green-600 text-lg font-bold'> &nbsp; Loading...</span>
+const ErrorTab = <span className='text-red-600 text-lg font-bold'> &nbsp; Something went wrong, Reload or Try again later...</span>
 
 export default TodoList;
--- frontend/src/hooks/UseApi.js
+++ frontend/src/hooks/UseApi.js
@@ -3,12 +3,19 @@ import { useGetTodosQuery, useUpdateToDoMutation, useDeleteToDoMutation, useAddT
 // Custom Hooks are very Important to write clean code and here we are exporting all our API functions and data from one hook
 
 const UseApi = () => {
-    const { data, error, isLoading } = useGetTodosQuery();
-    const [addTodo] = useAddTodoMutation();
-    const [deleteTodo] = useDeleteToDoMutation()
-    const [updateToDo] = useUpdateToDoMutation();
+
+    var mutationLoading = false, error;
+
+    const { data, isError, isLoading } = useGetTodosQuery();
+    const [addTodo, add] = useAddTodoMutation();
+    const [deleteTodo, del] = useDeleteToDoMutation()
+    const [updateToDo, upd] = useUpdateToDoMutation();
+
+    if (isError || add.isError || del.isError || upd.isError) { error = true }
+    if (add.isLoading || del.isLoading || upd.isLoading) { mutationLoading = true }
 
     return {
+        mutationLoading,
         data,
         error,
         isLoading,
